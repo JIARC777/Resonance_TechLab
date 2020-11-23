fb2ff7b8c5a9a10e4d35ec0c294e4706264f9821
carlsonh <45298981+carlsonh@users.noreply.github.com>
2020-11-23 15:50:48 -0700

    Initial Demo Scene implement

---
+++ Stencil_Buffer_Masking_HDRP/Assets/Scripts/Apply_Basic_Force.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apply_Basic_Force : MonoBehaviour
+{
+
+    private Rigidbody rig;
+
+    private Vector3 objectPos = new Vector3();
+
+    private float objectx;
+    private float objectY;
+    private float objectZ;
+
+    public float timeApplied;
+
+    public Vector3 forceApplied = new Vector3();
+
+    public int lerpMin;
+    public int lerpMax;
+
+    public char xyz;
+
+    public bool lerping = false;
+    public bool pausing = false;
+
+    private float interpolator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rig = GetComponent<Rigidbody>();
+        objectPos = transform.position;
+
+        objectx = objectPos.x;
+        objectY = objectPos.y;
+        objectZ = objectPos.z;
+        Debug.Log(objectx);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!pausing)
+            interpolator += timeApplied * Time.deltaTime;
+
+        if (!lerping)
+            rig.AddForce(forceApplied);
+        else
+            Lerping();
+
+    }
+
+    private void Lerping()
+    {
+        if (!pausing)
+        {
+            rig.isKinematic = false;
+
+            switch (xyz)
+            {
+                case 'x':
+
+                    transform.position = new Vector3(Mathf.Lerp(lerpMin, lerpMax, interpolator), objectY, objectZ);
+
+                    if (interpolator > 1)
+                        {
+
+                            int temp = lerpMax;
+                            lerpMax = lerpMin;
+                            lerpMin = temp;
+                            interpolator = 0f;
+
+                            transform.Rotate(new Vector3(0, 180, 0));                            
+                        }
+                    break;
+
+                case 'y':
+
+                    transform.position = new Vector3(objectx, Mathf.Lerp(lerpMin, lerpMax, interpolator), objectZ);
+
+                    if (interpolator > 1)
+                        {
+
+                            int temp = lerpMax;
+                            lerpMax = lerpMin;
+                            lerpMin = temp;
+                            interpolator = 0f;
+
+                            transform.Rotate(new Vector3(0, 180, 0));                            
+                        }
+                    break;
+
+                case 'z':
+
+                    transform.position = new Vector3(objectx, objectY, Mathf.Lerp(lerpMin, lerpMax, interpolator));
+
+                    if (interpolator > 1)
+                        {
+
+                            int temp = lerpMax;
+                            lerpMax = lerpMin;
+                            lerpMin = temp;
+                            interpolator = 0f;
+
+                            transform.Rotate(new Vector3(0, 180, 0));                            
+                        }
+                    break;
+
+                default:
+
+                    transform.position = new Vector3(Mathf.Lerp(lerpMin, lerpMax, interpolator), objectY, objectZ);
+
+                    if (interpolator > 1)
+                        {
+
+                            int temp = lerpMax;
+                            lerpMax = lerpMin;
+                            lerpMin = temp;
+                            interpolator = 0f;
+
+                            transform.Rotate(new Vector3(0, 180, 0));                            
+                        }
+                    break;
+            }            
+        }
+        else
+        {
+            rig.isKinematic = true;
+        }
+    }
+}
---
+++ Stencil_Buffer_Masking_HDRP/Assets/Scripts/Detection.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+
+    private GameObject parentDrone;
+
+    private Transform playerTracker;
+    private Transform turningObject;
+    private Transform otherObject;
+
+    public float lookTowards;
+    public float lookAway;
+    
+    private bool lookAtTracker = false;
+
+    private Apply_Basic_Force forces;
+
+    private void Awake()
+    {
+
+        parentDrone = GameObject.Find("Drone_Body");
+
+        turningObject = GameObject.Find("Turning_Object").GetComponent<Transform>();
+        playerTracker = GameObject.Find("Player_Tracker").GetComponent<Transform>();
+
+        forces = turningObject.GetComponent<Apply_Basic_Force>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == "Player")
+        {
+
+            otherObject = other.transform;            
+            LookAt();
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            
+            ReturnBack();
+                       
+        }
+    }
+
+    private void Update()
+    {
+
+        if (lookAtTracker)
+            PlayerTracker();
+        else
+            Turning();
+
+    }
+
+    private void LookAt()
+    {
+
+        StartCoroutine(Arriving());
+
+        forces.pausing = true;
+        lookAtTracker = true;
+
+        IEnumerator Arriving()
+        {
+
+            float beginX = playerTracker.transform.position.x;
+            float beginY = playerTracker.transform.position.y;
+            float beginZ = playerTracker.transform.position.z;
+
+            float endX = otherObject.transform.position.x;
+            float endY = otherObject.transform.position.y;
+            float endZ = otherObject.transform.position.z;
+
+            float time = 0;
+
+            while (time < lookTowards)
+            {
+
+                playerTracker.transform.position = new Vector3(Mathf.Lerp(beginX, endX, time / lookTowards), Mathf.Lerp(beginY, endY, time / lookTowards), Mathf.Lerp(beginZ, endZ, time / lookTowards));
+                parentDrone.transform.LookAt(playerTracker);
+                time += Time.deltaTime;
+                yield return null;
+
+            }
+
+            playerTracker.transform.position = otherObject.transform.position;            
+
+        }       
+    }
+
+    private void Turning()
+    {
+        
+        parentDrone.transform.LookAt(turningObject);
+
+    }
+
+    private void PlayerTracker()
+    {
+
+        playerTracker.transform.position = otherObject.transform.position;
+        parentDrone.transform.LookAt(playerTracker);
+
+    }
+
+    private void ReturnBack()
+    {
+
+        StartCoroutine(Returning());        
+
+        IEnumerator Returning()
+        {
+
+            float beginX = playerTracker.transform.position.x;
+            float beginY = playerTracker.transform.position.y;
+            float beginZ = playerTracker.transform.position.z;
+
+            float endX = turningObject.transform.position.x;
+            float endY = turningObject.transform.position.y;
+            float endZ = turningObject.transform.position.z;
+
+            float time = 0;
+
+            while(time < lookAway)
+            {
+
+                playerTracker.transform.position = new Vector3(Mathf.Lerp(beginX, endX, time / lookAway), Mathf.Lerp(beginY, endY, time / lookAway), Mathf.Lerp(beginZ, endZ, time / lookAway));
+                time += Time.deltaTime;
+                parentDrone.transform.LookAt(playerTracker);
+                yield return null;
+
+            }
+
+            otherObject = null;
+
+            playerTracker.transform.position = turningObject.transform.position;
+
+            lookAtTracker = false;
+            forces.pausing = false;
+        }
+    }
+}
