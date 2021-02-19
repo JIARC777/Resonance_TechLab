79341c0152f7700a2c99d97031adff1e01dd83b0
carlsonh <45298981+carlsonh@users.noreply.github.com>
2021-02-18 17:19:46 -0700

    Dave got refactored?!

---
+++ Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVEPathfollower.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DAVEPathfollower : IDaveState
+{
+    // Start is called before the first frame update
+    public void Initialize(DAVE dave)
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void UpdateLoop(DAVE dave)
+    {
+        
+    }
+}
