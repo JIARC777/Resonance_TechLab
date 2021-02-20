3cc1145154a832548053fb4b6845fbd34ab2a3f2
carlsonh <45298981+carlsonh@users.noreply.github.com>
2021-02-20 16:41:01 -0700

    DAVE

--- Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVE.cs
+++ Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVE.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 public interface IDaveState
 {
     void Initialize(DAVE dave);
     void UpdateCycle(DAVE dave);
+    void Exit();
 }
 public class DAVE : MonoBehaviour
 {
     [HideInInspector]
     // Reference to the current state of Dave
     public IDaveState currentState;
-    public DAVEData crossStateData;
+    ///public DAVEData crossStateData;
     // Set transform nodes in inspector
     public GameObject[] patrolPathNodes;
     // when Exiting Patrol, Patrol can access and set this as a marker, so it knows where to go when it starts patrolling again
-    public int curPathIndex;
+    [HideInInspector]
+    public int currentPatrolPathIndex;
     NavMeshAgent agent;
     // Store Sound Ids so that particles from the same sound are not called twice
     int[] soundIdHashes = new int[100];
@@ -36,9 +39,11 @@ public class DAVE : MonoBehaviour
     public delegate void ReachedDestination(DAVE dave);
     public event ReachedDestination ArrivedAtDestination;
     // Start is called before the first frame update
-    void Start()
-    {
-        crossStateData = new DAVEData();
+    void Awake()
+    {   
+        currentDestination = Vector3.zero;
+        agent = this.GetComponent<NavMeshAgent>();
+        //crossStateData = new DAVEData();
         // Start DAVE off in a patrol mode
         currentState = new DAVEPatroller();
         currentState.Initialize(this);
@@ -47,19 +52,27 @@ public class DAVE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((transform.position - currentDestination).magnitude <= agent.stoppingDistance)
+        // Debug.Log((currentDestination - transform.position).magnitude);
+        //Debug.Log(agent.stoppingDistance);
+        if ((currentDestination - transform.position).magnitude <= agent.stoppingDistance)
         {
-            ArrivedAtDestination(this);
+//             Debug.Log("Reached Destination");
+             ArrivedAtDestination(this);
         }
         currentState.UpdateCycle(this);
+//        Debug.Log(currentState.GetType().ToString());
+        
     }
 
     public void SetDestination(Vector3 newPointOfInterest)
 	{
+        Debug.Log("New Destination" + newPointOfInterest);
         currentDestination = newPointOfInterest;
+        // Debug.Log(newPointOfInterest);
         agent.SetDestination(currentDestination);
 	}
 
+    
     public void OnParticleCollision(GameObject noise)
     {
         if (noise.tag == "Noise")
@@ -70,11 +83,15 @@ public class DAVE : MonoBehaviour
             {
                 ActiveSound soundData = noise.GetComponent<ActiveSound>();
                 soundIdHashes[noiseID % 100] = noiseID;
-                Debug.Log("Beware! D.A.V.E. heard you");
+                //Debug.Log("Beware! D.A.V.E. heard you");
                 
                 // Check to see if the current State is not a DAVEInvestigator initialize state - This lets us add a transition case in DAVE itself while not creating a new state every time DAVE hears a sound
-                if (currentState.GetType().ToString() != "DAVEInvestigator")
-				{
+                var bNotInInvestigatorState = currentState.GetType().ToString() != "DAVEInvestigator";
+                Debug.Log(currentState.GetType().ToString());
+                if (bNotInInvestigatorState)
+                {
+                    currentState.Exit();
+                    //Debug.Log("Changing State to Investigator");
                     // Its worth noting now that a new investigator is created each time it transitions, so any old sounds are ignorned. 
                     //This shouldnt change much because the exit condition for the investigator is to essentially clear the list or get distracted by finding the player
                     currentState = new DAVEInvestigator();
@@ -85,5 +102,6 @@ public class DAVE : MonoBehaviour
             }
         }
     }
+    
 
 }
