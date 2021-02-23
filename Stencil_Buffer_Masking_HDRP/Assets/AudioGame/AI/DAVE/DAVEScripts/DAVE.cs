57540e2fc1d9ab339da4d1a995ce7fe5522faa8a
carlsonh <45298981+carlsonh@users.noreply.github.com>
2021-02-23 13:19:42 -0700

    D A V E

--- Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVE.cs
+++ Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVE.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 public interface IDaveState
 {
     void Initialize(DAVE dave);
+    //At some point we should verify that a local reference in each state (thisDave) makes a reference in the updateCycle unecessary
     void UpdateCycle(DAVE dave);
     void Exit();
 }
@@ -17,7 +18,10 @@ public class DAVE : MonoBehaviour
     // Set transform nodes in inspector
     public GameObject[] patrolPathNodes;
     // How far do you want a ping to travel
-    public float pingRadius = 7f;
+    // public float pingRadius = 7f;
+    public float attackRadius = 3f;
+    // How far is AI willing to travel before
+    public float chaseRadius = 5F;
     // when Exiting Patrol, Patrol can access and set this as a marker, so it knows where to go when it starts patrolling again
     [HideInInspector]
     public int currentPatrolPathIndex;
@@ -25,20 +29,26 @@ public class DAVE : MonoBehaviour
     // Store Sound Ids so that particles from the same sound are not called twice
     int[] soundIdHashes = new int[100];
     Vector3 currentDestination;
+    [HideInInspector]
+    public Vector3 lastKnownPlayerLocation;
+
+    // This acts as a switch to tell an initialized DAVEChaser class to update its target position
+    public bool pingFoundPlayer = false;
     GameObject currentPing;
 
     // Events
-    
-    
     // Called On OnParticleCollsion
     public delegate void SoundHeard(ActiveSound suspiciousNoise);
     public event SoundHeard HeardNoise;
+    
     // Called on how we want to handle damage players
     public delegate void Damaged(bool hitWithAMT);
     public event Damaged tookDamage;
+    
     // Called when agent reaches destination
     public delegate void ReachedDestination(DAVE dave);
     public event ReachedDestination ArrivedAtDestination;
+    
     // Start is called before the first frame update
     void Awake()
     {   
@@ -111,15 +121,39 @@ public class DAVE : MonoBehaviour
         currentPing = Instantiate(Resources.Load("PingSphere", typeof(GameObject)), transform.position + new Vector3(0, 2, 0), transform.rotation) as GameObject;
         PingSphere PingInfo = currentPing.GetComponent<PingSphere>();
        // PingInfo.maxRadius = pingRadius;
-        PingInfo.DetectedPlayer += EnterChaseState;
+        // Immediately sub to the Ping's event in case of collision - assume unsub on destroyed gameobject
+        PingInfo.DetectedPlayer += EngagePlayer;
         // if collision happens, this is quickly switched back on, if not it will stop the pinging process
     }
 
-    void EnterChaseState(Vector3 lastKnownPlayerLocation)
+    // We should 
+    void EngagePlayer(Vector3 knownPlayerLocation)
     {
-        currentState.Exit();
-        currentState = new DAVEChaser();
-        currentState.Initialize(this);
+        // Update Drone's last known player position
+        lastKnownPlayerLocation = knownPlayerLocation;
+        // Before chasing, check if we are close enough to attack
+        if ((knownPlayerLocation - this.transform.position).magnitude <= attackRadius)
+        {
+            // Entering Attack Condition
+            Debug.Log("Attack Player");
+        }
+        else
+        {
+            var bNotInChaseState = currentState.GetType().ToString() != "DAVEChaser";
+            if (bNotInChaseState)
+            {
+                currentState.Exit();
+                currentState = new DAVEChaser();
+                currentState.Initialize(this);
+                
+                pingFoundPlayer = false;
+            }
+            else
+            {
+                // pingFoundPlayer is not set to true until after state is initialized to avoid confusion with initialization function which by default sets the target
+                pingFoundPlayer = true;
+            }
+        }
         
     }
 
--- Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVEChaser.cs
+++ Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVEChaser.cs
@@ -2,22 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script is very similar to DAVEInvestigator - we might consider combining if we have time. Big difference is triggering event does not live on dave but any trigger colliders
 public class DAVEChaser : IDaveState
 {
     // Start is called before the first frame update
+    DAVE thisDave;
+    float timeSinceLastPing;
+
+    private float timeOfArrival;
+    // Since a ping takes a noticable finite amount of time, this keeps us from killing the state until we assume the ping returned void 
+    float timeTime = 5F;
     public void Initialize(DAVE dave)
     {
         Debug.Log("Player Found: Initialized Chaser");
+        thisDave = dave;
+        ChasePlayer(thisDave.lastKnownPlayerLocation);
+        thisDave.ArrivedAtDestination += Investigate;
     }
 
     // Update is called once per frame
     public void UpdateCycle(DAVE dave)
     {
+        if (thisDave.pingFoundPlayer)
+        {
+            // As soon as its true, set false
+            thisDave.pingFoundPlayer = false;
+            ChasePlayer(thisDave.lastKnownPlayerLocation);
+        }
+    }
+
+    public void ChasePlayer(Vector3 suspectedPlayerLocation)
+    {
+        
+        thisDave.SetDestination(suspectedPlayerLocation);
+    }
+
+    public void Investigate(DAVE dave)
+    {
+        timeSinceLastPing = Time.time;
+        // We asssume that if this ping returns a hit, we will reassign the position
+        thisDave.PingSurroundings();
     }
 
 
     public void Exit()
     {
-        
+        thisDave.ArrivedAtDestination -= Investigate;
     }
 }
