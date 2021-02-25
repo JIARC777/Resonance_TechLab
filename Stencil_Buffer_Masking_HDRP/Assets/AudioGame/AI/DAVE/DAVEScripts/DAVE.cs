31144a900f549102c42a8804b910c7aad8efdcb3
carlsonh <45298981+carlsonh@users.noreply.github.com>
2021-02-25 15:10:07 -0700

    DAVE WORKS!

--- Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVE.cs
+++ Stencil_Buffer_Masking_HDRP/Assets/AudioGame/AI/DAVE/DAVEScripts/DAVE.cs
@@ -27,6 +27,7 @@ public class DAVE : MonoBehaviour
     [HideInInspector]
     public bool waitingAtLocation = false;
 
+    
     [Header("Patrol State")]
     // Set transform nodes in inspector
     public GameObject[] patrolPathNodes;
@@ -34,19 +35,27 @@ public class DAVE : MonoBehaviour
     [HideInInspector]
     public int currentPatrolPathIndex;
 
+    
     [Header("Engagement Parameters")]
     public float attackRadius = 0.1f;
     // How long do we "deactivate" DAVE after he attacks;
     public float postAttackWaitTime = 10f;
-    // Whenever we get to determining how damage affects DAVE we can implement this flag to increase the speed and other factors - set to public in inspector for debugging/balancing 
-    public bool AgroMode;
+
+    private float attackTimestamp = 0f;
     // We need to move this but for simplicity I decrement this to create a game over state - how many times the player can get shot;
     public float playerHealth = 3f;
-    // How fast does DAVE travel when he's angry
-    public float AgroSpeedIncrease = 1f;
     // Switchs to update parameters easily inside update - should be related to when your toggle Agro mode
-    bool engageAgro;
-    bool disengageAgro;
+    
+
+    //TODO: To be implemented post V/S
+    // bool engageAgro;
+    // bool disengageAgro;
+    // How fast does DAVE travel when he's angry
+    // public float AgroSpeedIncrease = 1f;
+    // Whenever we get to determining how damage affects DAVE we can implement this flag to increase the speed and other factors - set to public in inspector for debugging/balancing 
+    // public bool AgroMode;
+
+    
 
     [Header("Pinging & Chasing")]
     public float pingRadius = 7f;
@@ -58,18 +67,26 @@ public class DAVE : MonoBehaviour
     public bool pingFoundPlayer = false;
     // Reference to the current pinging object to subscribe to it's event
     GameObject currentPing;
+    private float timeSinceLastPing = 0;
+    private float nearbySoundTimeout = 5F;
 
+    
+    
     [Header("Investiagtion")]
     //this ignores the case where dave investigates each of the x number of sound emissions occuring in a relativly tight radius within a small time frame
     public float ignoreCloselyPackedSoundsRadius = 1.5f;
     // We need to know the position of the last noise to know whether or not the noise we just heard was from an area close enough to the old sound to be ignored
     Vector3 lastNoisePosition = Vector3.zero;
 
+    
     [Header("Detectors")]
     // If we choose to go with a trigger based approach for detecting the player that extends past pings, assign the appropriate collider with a PlayerDetector script here so that we can subscribe to its event
     public PlayerDetector surroundingAreaDetector;
     
+
+    
     // Events
+    #region Events
     // Called On OnParticleCollsion
     public delegate void SoundHeard(ActiveSound suspiciousNoise);
     public event SoundHeard HeardNoise;
@@ -81,6 +98,12 @@ public class DAVE : MonoBehaviour
     // Called when agent reaches destination
     public delegate void ReachedDestination(DAVE dave);
     public event ReachedDestination ArrivedAtDestination;
+    
+    // Called when agent reaches destination
+    public delegate void PingExit(DAVE dave);
+    public event PingExit PingExited;
+    
+    #endregion
     
     // Start is called before the first frame update
     void Awake()
@@ -108,8 +131,11 @@ public class DAVE : MonoBehaviour
 //             Debug.Log("Reached Destination");
              ArrivedAtDestination(this);
         }
-        // Any state update code is run here
-        currentState.UpdateCycle(this);
+        
+        //
+        var bIsWaitingOnAttackAnimation = Time.time > attackTimestamp + postAttackWaitTime;
+        if(bIsWaitingOnAttackAnimation)
+            currentState.UpdateCycle(this);
 //      Debug.Log(currentState.GetType().ToString());
         
     }
@@ -118,7 +144,7 @@ public class DAVE : MonoBehaviour
 	{
         // New destination incoming, no longer waiting - make sure to call set destination after any wait periods (I think we already do this)
         waitingAtLocation = false;
-        Debug.Log("New Destination" + newPointOfInterest);
+        // Debug.Log("New Destination" + newPointOfInterest);
         currentDestination = newPointOfInterest;
         // Debug.Log(newPointOfInterest);
         agent.SetDestination(currentDestination);
@@ -139,6 +165,13 @@ public class DAVE : MonoBehaviour
                 soundIdHashes[noiseID % 100] = noiseID;
                 //Debug.Log("Beware! D.A.V.E. heard you");
                 // Check to make sure that the sound is not too close to the last sound we heard
+
+                var bCloseSoundHasTimedOut = Time.time > timeSinceLastPing + nearbySoundTimeout;
+                if (bCloseSoundHasTimedOut)
+                {
+                    lastNoisePosition = Vector3.positiveInfinity;
+                }
+                
                 var bSoundTooCloseToLastSound = (soundData.soundLocation - lastNoisePosition).magnitude <= ignoreCloselyPackedSoundsRadius;
                 lastNoisePosition = soundData.soundLocation;
                 if (bSoundTooCloseToLastSound)
@@ -160,7 +193,7 @@ public class DAVE : MonoBehaviour
                     currentState.Initialize(this);
                 }
                 // Call the event (after transitioning, so the investigator can pick up the event call)
-                HeardNoise(soundData);
+                HeardNoise?.Invoke(soundData);
             }
         }
     }
@@ -176,18 +209,21 @@ public class DAVE : MonoBehaviour
         // if collision happens, this is quickly switched back on, if not it will stop the pinging process
     }
 
-    // We should 
+    
+    
+    
+    // Fires on event, player detected by ping
     void EngagePlayer(Vector3 knownPlayerLocation)
     {
         // Update Drone's last known player position
         lastKnownPlayerLocation = knownPlayerLocation;
         // Before chasing, check if we are close enough to attack
         var bCanAttackPlayer = (this.transform.position - knownPlayerLocation).magnitude <= attackRadius;
-        Debug.Log("Player Distance Away: " + (this.transform.position - knownPlayerLocation).magnitude);
+        // Debug.Log("Player Distance Away: " + (this.transform.position - knownPlayerLocation).magnitude);
         if (bCanAttackPlayer)
-        {
+        {//Can attack player right now, do so
             // Entering Attack Condition
-            Debug.Log("Attack Player");
+            // Debug.Log("Attack Player");
             playerHealth--;
             if (playerHealth <= 0)
 			{
@@ -197,24 +233,25 @@ public class DAVE : MonoBehaviour
             
         }
         else
-        {
+        { //Can't attack player right now, must move to them
             var bNotInChaseState = currentState.GetType().ToString() != "DAVEChaser";
             if (bNotInChaseState)
-            {
+            {//Must transition to chaser state to go to player location
                 // Bit of a fun little bug I found here that explains alot of behavior we've seen - So we call exit on DAVEInvestigator in most cases which in turn by default activates a DAVEPatroller in the background referenced to the current state, so that is why we have a tendency to return to the patrol state all the time;
                 // Since its initialized and no one ever calls exit on it before the reference to the current state is changed, whenever it becomes Initialized I assume it will stay initialized for the durration of the run
                 currentState.Exit();
-                // Check to assume we hopped into this hidden patrol state and call exit on the patroller
+                ///FIX: Edge case, if exiting investigator, it automatically starts a patroller, exit that too
                 if (currentState.GetType().ToString() == "DAVEPatroller")
                     currentState.Exit();
+
                 currentState = new DAVEChaser();
                 currentState.Initialize(this);
-                
+
                 pingFoundPlayer = false;
             }
             else
-            {
-                // pingFoundPlayer is not set to true until after state is initialized to avoid confusion with initialization function which by default sets the target
+            {//Currently in chase state
+                //This re-ping found player, inform chaser to pathfind
                 pingFoundPlayer = true;
             }
         }
@@ -222,14 +259,24 @@ public class DAVE : MonoBehaviour
     }
 
     // We might have to implement a DAVEInvestigator style waiting system if we move this logic, but for now we can leverage a coroutine to pause DAVE after he attacks a player, giving them a chance to escape
+    /// <summary>
+    /// DAVE is attacking the player, wait for him to finish then exit to patroller
+    /// </summary>
+    /// <returns></returns>
     IEnumerator PauseActionAfterAttack()
-	{
+    {
+        attackTimestamp = Time.time;
+        Debug.Log("<color=yellow> Attack time happens now</color>");
         yield return new WaitForSeconds(postAttackWaitTime);
+        currentState.Exit();
         currentState = new DAVEPatroller();
         currentState.Initialize(this);
     }
 
-    // remove this when we have a nicer player health system
+    
+    /// <summary>
+    /// Player has died and the level should restart
+    /// </summary>
     void RestartLevel()
 	{
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
