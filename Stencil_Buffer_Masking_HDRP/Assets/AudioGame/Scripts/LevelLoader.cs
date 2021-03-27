c02a74090afae680b3941694a20155054bf935a5
carlsonh <carlsonh>
2021-03-27 16:19:08 -0700

    Partial Re-Sort
    Partial resort for Vertical / Slice

---
+++ Stencil_Buffer_Masking_HDRP/Assets/AudioGame/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteAlways]
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private string[] sceneNames;
+
+    private void Awake()
+    {
+
+
+        if (Application.isPlaying)
+        {
+            foreach (string sceneName in sceneNames)
+            {
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            }
+        }
+        else
+        {
+            foreach (string sceneName in sceneNames)
+            {
+                EditorSceneManager.OpenScene(sceneName, OpenSceneMode.Additive);
+            }
+        }
+    }
+    
+}
