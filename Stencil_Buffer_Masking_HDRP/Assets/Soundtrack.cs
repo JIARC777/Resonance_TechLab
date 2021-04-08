98168f71e69deca6e29ada97ed5553a085dce839
Jeremy Ivan <59781543+JIARC777@users.noreply.github.com>
2021-04-07 23:02:33 -0700

    All Walls tagged as map hazard in vertical slice - Sound FX for all prefabbed assets

---
+++ Stencil_Buffer_Masking_HDRP/Assets/Soundtrack.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundtrack : MonoBehaviour
+{
+    AudioSource source;
+    float timeToPlayNext;
+    bool updateTime = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying && updateTime)
+		{
+            timeToPlayNext = Time.time + Random.Range(100, 200);
+            updateTime = false;
+		}
+        if (Time.time >= timeToPlayNext)
+		{
+            source.Play();
+            updateTime = true;
+		}
+    }
+}
